45ee03d3de275015c1f4a62354a3109486a4611f
Jordan Castelloe <jordan.castelloe@gmail.com>
2019-08-27 10:55:27 -0400

    deleted old comments and added new comments

--- src/scripts/domPrinter.js
+++ src/scripts/domPrinter.js
@@ -1,14 +1,13 @@
 // Module that contains function to print to the DOM
 
 const domPrinter = {
+    // Accepts to parameters: a food from JSON Server and a food from the API
   printSingleFood: (foodFromJSONServer, foodFromExternalAPI) => {
     if (foodFromExternalAPI.product.ingredients_text) {
       foodFromJSONServer.ingredients = foodFromExternalAPI.product.ingredients_text;
     } else {
       foodFromJSONServer.ingredients = "no ingredients listed";
     }
-
-
     // Build HTML string for individual food
     const htmlString = htmlBuilder.buildFoodCard(foodFromJSONServer);
     // Add HTML string to DOM
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,33 +1,12 @@
-// Once you have retrieved all of the foods from your own Food API, display each one of them in the DOM. Create an HTML representation of each food which will display the name of the food, its type, and its ethnicity.
 
-// Create a DOM element in your index.html with a class of foodList.
-// Use the template code below to take the data and print it to the DOM.
-// Example fetch call using functions
-
-// fetch("http://localhost:8088/food")
-//     .then(foods => foods.json())
-//     .then(parsedFoods => {
-
-//         parsedFoods.forEach(food => {
-//            //Print foods to DOM
-//            document.querySelector("#container").innerHTML += `<div class="flex">
-//            <h3>${food.name}</h3>
-//            <p>${food.category}</p>
-//            <p>${food.ethnicity}</p>
-//            <p>${}</p>
-
-//            </div>
-//            `
-//         })
-//     })
-
-   apiManager.getAllFoods()
-    .then(myParsedFoods => {
-        myParsedFoods.forEach(food => {
-            // Now fetch the food from the Food API
-            apiManager.getIngredientsForSingleFood(food)
-                .then(productInfo => {
-                   domPrinter.printSingleFood(food, productInfo)
-                })
-        })
-    })
+// Get foods from JSON Server
+apiManager.getAllFoods().then(myParsedFoods => {
+  // Loop through the foods from JSON Server
+  myParsedFoods.forEach(food => {
+    // Now fetch the food from the Food API
+    apiManager.getIngredientsForSingleFood(food).then(productInfo => {
+      // Print each food to the DOM
+      domPrinter.printSingleFood(food, productInfo);
+    });
+  });
+});
