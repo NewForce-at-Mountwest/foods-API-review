16b5ae1a224c1cde645127dc44196cf37904b144
Tommy Spurlock <tspurloc@gmail.com>
2019-08-27 09:32:35 -0400

    did the thing

---
+++ src/scripts/script.js
@@ -0,0 +1,20 @@
+// Once you have retrieved all of the foods from your own Food API, display each one of them in the DOM. Create an HTML representation of each food which will display the name of the food, its type, and its ethnicity.
+
+// Create a DOM element in your index.html with a class of foodList.
+// Use the template code below to take the data and print it to the DOM.
+// Example fetch call using functions
+
+fetch("http://localhost:8088/food")
+    .then(foods => foods.json())
+    .then(parsedFoods => {
+
+        parsedFoods.forEach(food => {
+           //Print foods to DOM
+           document.querySelector("#container").innerHTML += `<div class="flex">
+           <h3>${food.name}</h3>
+           <p>${food.category}</p>
+           <p>${food.ethnicity}</p>
+           </div>
+           `
+        })
+    })
